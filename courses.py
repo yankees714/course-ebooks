434fcf9d436309fa65474f473847901938e536c4
Andrew Daniels <adaniels@quantopian.com>
2018-01-24 20:16:03 -0500

    Cleanup

--- courses.py
+++ courses.py
@@ -1,14 +1,12 @@
 from markov import MarkovChainer
 
+
 #source = "./courses.txt"
 source = "./spring2014.txt"
 
-f = open(source,"r")
-descriptions = f.read()
-f.close()
-
 generator = MarkovChainer(2)
-generator.add_text(descriptions)
+with open(source) as f:
+    generator.add_text(f.read())
 
 generated_description = generator.generate_sentence()
 while len(generated_description) < 100:
